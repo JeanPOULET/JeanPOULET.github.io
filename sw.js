fa0bea135ab62b438854472386593eca7572650c
JeanPOULET <57538596+JeanPOULET@users.noreply.github.com>
2020-11-23 11:51:12 +0100

    oue

--- sw.js
+++ sw.js
@@ -25,27 +25,14 @@ self.addEventListener('fetch', function (event) {
     event.respondWith(
       caches.open('ginko-dynamic').then(function (cache) {
         return cache.match(event.request).then(function (response) {
-          var fetchPromise = fetch(event.request).then(function (networkResponse) {
-            cache.put(event.request, networkResponse.clone());
-            return networkResponse;
-          });
-          return response || fetchPromise;
+          return (
+            response ||
+            fetch(event.request).then(function (response) {
+              cache.put(event.request, response.clone());
+              return response;
+            })
+          );
         });
       }),
     );
   });
-
-/*self.addEventListener('fetch', (e) => {
-    e.respondWith(
-      caches.match(e.request).then((r) => {
-            console.log('[Service Worker] Fetching resource: '+e.request.url);
-        return r || fetch(e.request).then((response) => {
-                  return caches.open(cacheName).then((cache) => {
-            console.log('[Service Worker] Caching new resource: '+e.request.url);
-            cache.put(e.request, response.clone());
-            return response;
-          });
-        });
-      })
-    );
-  });*/
