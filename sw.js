ad7aa2d340baf4ad4c6acd9e8e5e3b30de1b7df5
JeanPOULET <57538596+JeanPOULET@users.noreply.github.com>
2020-11-23 11:18:24 +0100

    oue

--- sw.js
+++ sw.js
@@ -1,20 +1,20 @@
 
 var cacheName = "ginko-v1";
-var cache = [];
+var contentToCache = [];
 var icons=["32","64","96","128","168","180","192","256","512"];
 
 for(var i=0; i<icons.length; i++) {
-    cache.push('/icons/icon-'+icons[i]+'.png');
+    contentToCache.push('/icons/icon-'+icons[i]+'.png');
 }
 
-cache.push('index.html');
+contentToCache.push('index.html');
 
 self.addEventListener('install', (e) => {
     console.log('[Service Worker] Install');
     e.waitUntil(
         caches.open(cacheName).then((cache) => {
               console.log('[Service Worker] Caching all: app shell and content');
-          return cache.addAll(cache);
+          return cache.addAll(contentToCache);
         })
       );
 });
