c2ab6de0f1841800ce43ea58d5f7be03e966262c
JeanPOULET <57538596+JeanPOULET@users.noreply.github.com>
2020-11-23 11:14:07 +0100

    oue

--- sw.js
+++ sw.js
@@ -6,14 +6,13 @@ var icons=["32","64","96","128","168","180","192","256","512"];
 for(var i=0; i<icons.length; i++) {
     cacheIcons.push('/icons/icon-'+icons[i]+'.png');
 }
-var contentToCache = appShellFiles.concat(cacheIcons);
 
 self.addEventListener('install', (e) => {
     console.log('[Service Worker] Install');
     e.waitUntil(
         caches.open(cacheName).then((cache) => {
               console.log('[Service Worker] Caching all: app shell and content');
-          return cache.addAll(contentToCache);
+          return cache.addAll(cacheIcons);
         })
       );
 });
